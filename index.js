30da8c86958e417ae6f20c4ee818c1f081cfcb11
Stephen <hellostephenwm@gmail.com>
2024-05-15 18:39:24 -0400

    Added checks in POST method to ensure name and number as passed and the name is unique

--- index.js
+++ index.js
@@ -60,8 +60,24 @@ const generateId = () => {
 	return isUnique ? newId : generateId();
 };
 
+const checkPerson = (name) => {
+	return persons.some(
+		(person) => person.name.toLowerCase() === name.toLowerCase()
+	);
+};
+
 app.post("/api/persons/", (request, response) => {
 	const body = request.body;
+
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: "name or number is missing",
+		});
+	} else if (checkPerson(body.name)) {
+		return response.status(400).json({
+			error: "name must be unique",
+		});
+	}
 
 	const person = {
 		name: body.name,
