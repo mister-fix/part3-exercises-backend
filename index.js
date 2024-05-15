6e8f080102442e4d4c54806acd59b72200379b9f
Stephen <hellostephenwm@gmail.com>
2024-05-15 16:14:32 -0400

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/", (request, response) => {
+	response.send("<h1>Hello World</h1>");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
