4e03fff438a2fdbc7a2c8de9de95789323d837d4
Stephen <hellostephenwm@gmail.com>
2024-05-21 22:40:35 -0400

    added morgan token for POST request bodies to log the payload

--- index.js
+++ index.js
@@ -27,7 +27,18 @@ let persons = [
 ];
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (request) => {
+	if (request.method === "POST") {
+		return JSON.stringify(request.body);
+	}
+
+	return "";
+});
+
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/info", (request, response) => {
 	response.send(
